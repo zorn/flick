06e9c5a7092f528b3c887eac0e757f3aec5e1e9c
Mike Zornek <zorn@zornlabs.com>
2024-05-22 08:41:13 -0400

    Add required alias.

--- lib/flick_web/live_view_pipes.ex
+++ lib/flick_web/live_view_pipes.ex
@@ -3,6 +3,8 @@ defmodule FlickWeb.LiveViewPipes do
   A collection of functions to help express pipes when processing live view responses.
   """
 
+  alias Phoenix.LiveView.Socket
+
   @spec ok(Socket.t()) :: {:ok, Socket.t()}
   def ok(socket), do: {:ok, socket}
 end
