409ba02a34605cc394a359077293bf7d840e062b
Mike Zornek <zorn@zornlabs.com>
2024-07-15 21:34:49 -0400

    Update page contents.

--- lib/flick_web/live/ballots/index_live.ex
+++ lib/flick_web/live/ballots/index_live.ex
@@ -19,28 +19,31 @@ defmodule FlickWeb.Ballots.IndexLive do
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <div>
+    <div class="prose">
       <p>
         <.link navigate={~p"/ballots/new"} class="underline">Create a new ballot.</.link>
       </p>
       <p>A list of known ballots.</p>
-      <.table id="ballots" rows={@ballots} row_id={&"ballot-row-#{&1.id}"}>
-        <:col :let={ballot} label="title">
-          <%= ballot.question_title %>
-        </:col>
-        <:col :let={ballot} label="published at">
-          <%= if ballot.published_at do %>
-            <%= ballot.published_at %>
-            <.link navigate={~p"/vote/#{ballot.id}"}>Voting Page</.link>
-          <% else %>
-            Not Published
-          <% end %>
-        </:col>
-        <:col :let={ballot} label="details">
-          <.link navigate={~p"/ballots/#{ballot.id}"}>View Details</.link>
-        </:col>
-      </.table>
     </div>
+
+    <.table id="ballots" rows={@ballots} row_id={&"ballot-row-#{&1.id}"}>
+      <:col :let={ballot} label="Title">
+        <%= ballot.question_title %>
+      </:col>
+      <:col :let={ballot} label="Published">
+        <%= if ballot.published_at do %>
+          <%= ballot.published_at %>
+        <% else %>
+          Not Published
+        <% end %>
+      </:col>
+      <:col :let={ballot}>
+        <.link :if={ballot.published_at} navigate={~p"/vote/#{ballot.id}"}>Voting Page</.link>
+      </:col>
+      <:col :let={ballot}>
+        <.link navigate={~p"/ballots/#{ballot.id}"}>View Details</.link>
+      </:col>
+    </.table>
     """
   end
 end
