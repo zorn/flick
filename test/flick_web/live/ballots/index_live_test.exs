fb83d5539a078ae14574fcad0c34dd364db28630
Mike Zornek <zorn@zornlabs.com>
2024-07-16 20:59:48 -0400

    chore: add `tiny_maps` to help with condensed test syntax (#16)

--- test/flick_web/live/ballots/index_live_test.exs
+++ test/flick_web/live/ballots/index_live_test.exs
@@ -1,7 +1,7 @@
 defmodule FlickWeb.Ballots.IndexLiveTest do
   use FlickWeb.ConnCase, async: true
 
-  test "renders list of ballots", %{conn: conn} do
+  test "renders list of ballots", ~M{conn} do
     ballots = Enum.map(1..3, fn _ -> ballot_fixture() end)
     assert {:ok, view, _html} = live(conn, ~p"/ballots")
     assert has_element?(view, "table#ballots")
--- test/flick_web/live/ballots/viewer_live_test.exs
+++ test/flick_web/live/ballots/viewer_live_test.exs
@@ -1,13 +1,13 @@
 defmodule FlickWeb.Ballots.ViewerLiveTest do
   use FlickWeb.ConnCase, async: true
 
-  test "renders ballot details", %{conn: conn} do
+  test "renders ballot details", ~M{conn} do
     ballot = ballot_fixture()
     assert {:ok, view, _html} = live(conn, ~p"/ballots/#{ballot}")
     assert has_element?(view, "#ballot-question-title", ballot.question_title)
   end
 
-  test "responds with 404 when no ballot is found", %{conn: conn} do
+  test "responds with 404 when no ballot is found", ~M{conn} do
     assert_raise Ecto.NoResultsError, fn ->
       live(conn, ~p"/ballots/#{Ecto.UUID.generate()}")
     end
