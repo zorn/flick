92aef7f7594ba824aea073ac56a6514b7a27dfdc
Mike Zornek <zorn@zornlabs.com>
2024-07-30 07:48:13 -0400

    Update index page and add tests.

---
+++ test/flick_web/live/index_live_test.exs
@@ -0,0 +1,26 @@
+defmodule FlickWeb.IndexLiveTest do
+  @moduledoc """
+  """
+
+  use FlickWeb.ConnCase, async: true
+
+  test "renders the index page", ~M{conn} do
+    assert {:ok, _view, _html} = live(conn, ~p"/")
+  end
+
+  test "contains welcome copy", ~M{conn} do
+    {:ok, view, _html} = live(conn, ~p"/")
+
+    expected_copy =
+      """
+      Flick is an application that allows you to create ballots that capture ranked votes.
+      """
+
+    assert element(view, "#welcome-copy") |> render() =~ expected_copy
+  end
+
+  test "contains link to create a ballot", ~M{conn} do
+    {:ok, view, _html} = live(conn, ~p"/")
+    assert has_element?(view, "a[href='/create-ballot']", "Create a ballot")
+  end
+end
