691b81f5e0281ed37031374341ee6c709e521ff0
Mike Zornek <zorn@zornlabs.com>
2024-08-05 21:44:57 -0400

    Add `id` for testing help.

--- lib/flick_web/live/vote/vote_capture_live.ex
+++ lib/flick_web/live/vote/vote_capture_live.ex
@@ -65,7 +65,7 @@ defmodule FlickWeb.Vote.VoteCaptureLive do
   def render(assigns) do
     ~H"""
     <div>
-      <div><%= @ballot.question_title %></div>
+      <div id="question-title"><%= @ballot.question_title %></div>
 
       <.simple_form for={@form} phx-change="validate" phx-submit="save">
         <%!-- I wonder if we should drop this hidden and just inject the id manually? --%>
