2990391d71b703eff90d5cb8711c18bd64bc9529
Mike Zornek <zorn@zornlabs.com>
2024-08-12 22:12:15 -0400

    Fix tests with new id.

--- test/flick_web/live/ballots/index_live_test.exs
+++ test/flick_web/live/ballots/index_live_test.exs
@@ -8,10 +8,10 @@ defmodule FlickWeb.Ballots.IndexLiveTest do
   test "renders list of ballots", ~M{conn} do
     ballots = Enum.map(1..3, fn _ -> ballot_fixture() end)
     assert {:ok, view, _html} = live(conn, ~p"/admin/ballots")
-    assert has_element?(view, "table#ballots")
+    assert has_element?(view, "tbody#ballots")
 
     for ballot <- ballots do
-      row_selector = "table#ballots tr#ballot-row-#{ballot.id}"
+      row_selector = "tbody#ballots tr#ballot-row-#{ballot.id}"
       assert has_element?(view, row_selector, ballot.question_title)
       assert has_element?(view, row_selector, "Not Published")
     end
