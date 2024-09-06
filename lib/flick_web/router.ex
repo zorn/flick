74888224dea7328a9a50ce6e0ee303ed54eccce1
Mike Zornek <zorn@zornlabs.com>
2024-09-05 21:52:10 -0400

    Add URL to comment.

--- lib/flick_web/router.ex
+++ lib/flick_web/router.ex
@@ -12,6 +12,7 @@ defmodule FlickWeb.Router do
 
     # Tailwind uses SVG data URLs for icons,
     # so we need to allow them with `img-src`.
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
     plug :put_secure_browser_headers, %{
       "content-security-policy" => "default-src 'self'; img-src 'self' data:"
     }
