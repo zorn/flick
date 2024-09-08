a3bc0fe784842ca7b4035abad944ab411634ddc1
Mike Zornek <zorn@zornlabs.com>
2024-09-08 13:55:57 -0400

    Requiring all decorators with `@impl` are expressive of the behaviours they are referencing.

--- lib/flick/application.ex
+++ lib/flick/application.ex
@@ -5,7 +5,7 @@ defmodule Flick.Application do
 
   use Application
 
-  @impl true
+  @impl Application
   def start(_type, _args) do
     Appsignal.Phoenix.LiveView.attach()
 
@@ -30,7 +30,7 @@ defmodule Flick.Application do
 
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
-  @impl true
+  @impl Application
   def config_change(changed, _new, removed) do
     FlickWeb.Endpoint.config_change(changed, removed)
     :ok
