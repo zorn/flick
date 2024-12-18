e3d336a5e2fea82e91b2c813027eb713175c14d1
Mike Zornek <zorn@zornlabs.com>
2024-12-18 08:22:51 -0500

    Add page title and other copy cleanup.

--- lib/flick_web/live/index_live.ex
+++ lib/flick_web/live/index_live.ex
@@ -9,7 +9,9 @@ defmodule FlickWeb.IndexLive do
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    socket
+    |> assign(page_title: "Home")
+    |> ok()
   end
 
   @impl Phoenix.LiveView
@@ -18,7 +20,11 @@ defmodule FlickWeb.IndexLive do
     <div class="prose prose-p:leading-6 prose-li:my-0">
       <section>
         <p>
-          Many traditional voting systems ask for a single choice per voter, and the winner is chosen based on the number of votes cast for any single choice. In a ranked vote, voters provide ranked preferences instead of selecting a single choice, which ultimately results in a better consensus outcome.
+          Many traditional voting systems ask for a single choice per voter, and the winner is chosen based on the number of votes cast for any single choice.
+        </p>
+
+        <p>
+          In a <strong>ranked vote</strong>, voters provide ranked preferences instead of selecting a single choice, which ultimately results in a <strong>better consensus outcome</strong>.
         </p>
 
         <p>
@@ -38,11 +44,11 @@ defmodule FlickWeb.IndexLive do
       </section>
 
       <section>
-        <p class="font-bold">Use cases:</p>
+        <p class="font-bold">What is ranked voting good for?</p>
 
         <ul>
           <li>Pick a book for a book club.</li>
-          <li>Select a movie for movie night.</li>
+          <li>Select a movie for family night.</li>
           <li>Choose a vacation destination for a large group.</li>
           <li>Decide on a new logo for a company.</li>
           <li>Prioritize technical debt projects.</li>
