cb7d71de2ea903e00d0a1b67aa9bb966b1616fe5
Mike Zornek <zorn@zornlabs.com>
2024-12-18 08:40:11 -0500

    Add typespec.

--- lib/flick_web/components/ui.ex
+++ lib/flick_web/components/ui.ex
@@ -6,9 +6,13 @@ defmodule FlickWeb.UI do
   use Phoenix.Component
   use FlickWeb, :verified_routes
 
+  alias Phoenix.LiveView.Rendered
+  alias Phoenix.LiveView.Socket
+
   slot :inner_block, required: true
   attr :rest, :global
 
+  @spec page_column(Socket.assigns()) :: Rendered.t()
   def page_column(assigns) do
     ~H"""
     <div class="max-w-2xl mx-auto px-4">
