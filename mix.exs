341374cf99326f623889123e2e258792e29df43e
Mike Zornek <zorn@zornlabs.com>
2024-12-30 13:27:33 -0500

    bump elixir and erlang

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Flick.MixProject do
     [
       app: :flick,
       version: "0.1.0",
-      elixir: "~> 1.17.3",
+      elixir: "~> 1.18.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
