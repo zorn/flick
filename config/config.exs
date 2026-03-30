a85e1c67b4e758f0f8e4201619a284cf205970f7
Mike Zornek <zorn@zornlabs.com>
2026-03-30 11:51:43 -0400

    Update `tailwind` config.

--- config/config.exs
+++ config/config.exs
@@ -53,14 +53,13 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.4.0",
+  version: "4.1.12",
   flick: [
     args: ~w(
-      --config=tailwind.config.js
-      --input=css/app.css
-      --output=../priv/static/assets/app.css
+      --input=assets/css/app.css
+      --output=priv/static/assets/css/app.css
     ),
-    cd: Path.expand("../assets", __DIR__)
+    cd: Path.expand("..", __DIR__)
   ],
   storybook: [
     args: ~w(
