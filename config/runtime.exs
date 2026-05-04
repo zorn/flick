9e9498a98b720d134bda4efa7c042ebd112a069b
Mike Zornek <zorn@zornlabs.com>
2026-05-04 14:49:29 -0400

    Revert port configuration pattern to avoid nested overwrites that would miss the configured port.

--- config/runtime.exs
+++ config/runtime.exs
@@ -20,8 +20,6 @@ if System.get_env("PHX_SERVER") do
   config :flick, FlickWeb.Endpoint, server: true
 end
 
-config :flick, FlickWeb.Endpoint, http: [port: String.to_integer(System.get_env("PORT", "4000"))]
-
 if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
@@ -57,6 +55,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOSTNAME") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :flick, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
@@ -67,7 +66,8 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
     ],
     secret_key_base: secret_key_base
 
