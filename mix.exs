7732908254b4b845b5e8bb79daeed9671e3e566c
Mike Zornek <zorn@zornlabs.com>
2026-05-04 19:07:25 -0400

    Update Elixir and Erlang versions.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Flick.MixProject do
     [
       app: :flick,
       version: "0.1.0",
-      elixir: "~> 1.18.0",
+      elixir: "~> 1.19.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
