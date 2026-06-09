5b425ee5052702c842b7791cb2c7f29ff57a2956
Mike Zornek <zorn@zornlabs.com>
2026-06-08 21:55:39 -0400

    Fix credo warnings.

--- lib/flick/name_generator.ex
+++ lib/flick/name_generator.ex
@@ -1,7 +1,10 @@
 defmodule Flick.NameGenerator do
+  @moduledoc "Provides functions to generate random person names for use in seeds and test data."
+
   @first_names ~w(Alice Bob Carol David Emma Frank Grace Henry Isabel James Karen Leo Maria Nora Oscar Paula Quinn Rachel Sam Tara)
   @last_names ~w(Anderson Brown Clark Davis Evans Foster Garcia Harris Irving Johnson Kelly Lopez Martinez Nelson Owen Parker Quinn Rivera Scott Taylor)
 
+  @spec person_name() :: String.t()
   def person_name do
     "#{Enum.random(@first_names)} #{Enum.random(@last_names)}"
   end
